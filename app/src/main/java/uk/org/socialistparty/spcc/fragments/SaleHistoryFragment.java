b6e1f4912f53c23e093fb07997dbd8b465a5f719
dgilmore91 <daniel.gilmore@solarwinds.com>
2018-05-22 13:41:59 +0100

    Removed unnecessary stuff

--- app/src/main/java/uk/org/socialistparty/spcc/fragments/SaleHistoryFragment.java
+++ app/src/main/java/uk/org/socialistparty/spcc/fragments/SaleHistoryFragment.java
@@ -1,7 +1,5 @@
 package uk.org.socialistparty.spcc.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -17,48 +15,11 @@ import uk.org.socialistparty.spcc.R;
 import uk.org.socialistparty.spcc.data.Sale;
 import uk.org.socialistparty.spcc.util.SalesRecyclerAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SaleHistoryFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SaleHistoryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SaleHistoryFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
     private List<Sale> sales;
-
-    private RecyclerView recyclerView;
-    private RecyclerView.LayoutManager layoutManager;
-    private RecyclerView.Adapter recyclerAdapter;
-
-    private OnFragmentInteractionListener mListener;
 
     public SaleHistoryFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SaleHistoryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SaleHistoryFragment newInstance(String param1, String param2) {
-        SaleHistoryFragment fragment = new SaleHistoryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
@@ -99,53 +60,15 @@ public class SaleHistoryFragment extends Fragment {
     }
 
     private void initRecyclerView(View containerView){
-        recyclerView = (RecyclerView) containerView.findViewById(R.id.sales_history_recycler_view);
+        RecyclerView recyclerView = containerView.findViewById(R.id.sales_history_recycler_view);
 
         recyclerView.setHasFixedSize(true);
 
-        layoutManager = new LinearLayoutManager(this.getContext());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getContext());
         recyclerView.setLayoutManager(layoutManager);
 
-        recyclerAdapter = new SalesRecyclerAdapter(sales);
+        RecyclerView.Adapter recyclerAdapter = new SalesRecyclerAdapter(sales);
         recyclerView.setAdapter(recyclerAdapter);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
--- app/src/main/java/uk/org/socialistparty/spcc/util/SalesRecyclerAdapter.java
+++ app/src/main/java/uk/org/socialistparty/spcc/util/SalesRecyclerAdapter.java
@@ -9,18 +9,15 @@ import android.widget.TextView;
 import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 import uk.org.socialistparty.spcc.R;
 import uk.org.socialistparty.spcc.data.Sale;
 
 public class SalesRecyclerAdapter extends RecyclerView.Adapter<SalesRecyclerAdapter.ViewHolder> {
     private List<Sale> sales;
+    private Calendar calendar = Calendar.getInstance();
+    private DateFormat formatter = java.text.DateFormat.getDateInstance(DateFormat.FULL);
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     static class ViewHolder extends RecyclerView.ViewHolder {
         ConstraintLayout itemBaseLayout;
         ViewHolder(ConstraintLayout v) {
@@ -29,23 +26,18 @@ public class SalesRecyclerAdapter extends RecyclerView.Adapter<SalesRecyclerAdap
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
     public SalesRecyclerAdapter(List<Sale> sales) {
         this.sales = sales;
     }
 
-    // Create new views (invoked by the layout manager)
     @Override
-    public SalesRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                   int viewType) {
-        // create a new view
+    public SalesRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         ConstraintLayout v = (ConstraintLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.sale_history_list_item, parent, false);
 
         return new ViewHolder(v);
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         Sale sale = sales.get(position);
@@ -54,9 +46,7 @@ public class SalesRecyclerAdapter extends RecyclerView.Adapter<SalesRecyclerAdap
         TextView paperView = baseLayout.findViewById(R.id.sale_history_list_item_paper_view);
         TextView fundView = baseLayout.findViewById(R.id.sale_history_list_item_fund_view);
 
-        Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(sale.getSaleDate());
-        DateFormat formatter = java.text.DateFormat.getDateInstance(DateFormat.FULL);
         String formatted = formatter.format(calendar.getTime());
 
         dateView.setText(formatted);
@@ -65,7 +55,6 @@ public class SalesRecyclerAdapter extends RecyclerView.Adapter<SalesRecyclerAdap
 
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return sales.size();
