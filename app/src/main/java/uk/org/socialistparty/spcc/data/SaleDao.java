dc5e6a00710708ed3a129228bb3891187e506bc2
dgilmore91 <daniel.gilmore@solarwinds.com>
2018-05-23 12:53:40 +0100

    Added DAO methods to return specific datasets

--- app/src/main/java/uk/org/socialistparty/spcc/data/SaleDao.java
+++ app/src/main/java/uk/org/socialistparty/spcc/data/SaleDao.java
@@ -12,6 +12,36 @@ public interface SaleDao {
     @Query("SELECT * FROM sales")
     List<Sale> getAll();
 
+    @Query("SELECT * FROM sales ORDER BY sale_date")
+    List<Sale> getAllOrderedByDate();
+
+    @Query("SELECT * FROM sales ORDER BY sale_date DESC")
+    List<Sale> getAllOrderedByDateDesc();
+
+    @Query("SELECT * FROM sales ORDER BY fund_raised")
+    List<Sale> getAllOrderedByFund();
+
+    @Query("SELECT * FROM sales ORDER BY fund_raised DESC")
+    List<Sale> getAllOrderedByFundDesc();
+
+    @Query("SELECT * FROM sales ORDER BY papers_sold")
+    List<Sale> getAllOrderedByPapers();
+
+    @Query("SELECT * FROM sales ORDER BY papers_sold DESC")
+    List<Sale> getAllOrderedByPapersDesc();
+
+    @Query("SELECT * FROM sales ORDER BY is_paid ")
+    List<Sale> getAllOrderedByPaid();
+
+    @Query("SELECT * FROM sales ORDER BY is_paid DESC")
+    List<Sale> getAllOrderedByPaidDesc();
+
+    @Query("SELECT * FROM sales WHERE is_paid")
+    List<Sale> getAllPaidSales();
+
+    @Query("SELECT * FROM sales WHERE NOT is_paid")
+    List<Sale> getAllUnpaidSales();
+
     @Query("SELECT * FROM sales WHERE sale_id IN (:saleIds)")
     List<Sale> loadAllByIds(int[] saleIds);
 
